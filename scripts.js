4845912515b5d26b1455775d4c4f6bfbe002049a
Lauren Reilly <LaurenReillyNet@gmail.com>
2018-08-22 16:38:36 -0400

    finished challenge

--- scripts.js
+++ scripts.js
@@ -31,7 +31,19 @@ function updateButton (){
 }
 
 function handleRangeUpdate(){
+    video[this.name] = this.value;
     console.log(this.value);
+    console.log(this.name);
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e){
+    const scrubTime = (e.offsetX/progress.offsetWidth) *video.duration;
+    video.currentTime = scrubTime;
 }
 
 /* hook up event listeners */
@@ -39,9 +51,16 @@ function handleRangeUpdate(){
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(button => button.addEventListener('click',skip))
 
-ranges.addEventListener('change', handleRangeUpdate);
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown",() => mouseup = true);
+progress.addEventListener("mouseup", () => mousedown = false);
